c18fd6a3719abc4b67bfeb9a8eaadd11ff440e2e
chris reynolds <chris@jazzsequence.com>
2013-01-02 14:44:40 -0700

    creates php file with plugin header

---
+++ press-page.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: Press Page
+git uri: https://github.com/jazzsequence/press-page
+Plugin URI: http://www.museumthemes.com
+Description: A WordPress plugin to display a custom post type for press features in a horizontal sliding scroll
+Version: 0.1
+Author: Arcane Palette Creative Design
+Author URI: http://arcanepalette.com/
+License: GPL3
+*/
+
+?>
